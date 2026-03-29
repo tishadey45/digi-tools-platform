16563fb98997ee689e7518cf2e0ca3b342a7902a
Tisha Dey <tishadey2025@outlook.com>
2026-03-30 00:47:21 +0600

    :pencil2: navbar section done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm sm:px-8">
+    <>
+      <div className="navbar bg-base-100 shadow-sm sm:px-8 px-4 md:px-10 lg:px-16 py-4 flex items-center justify-between">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -22,7 +24,9 @@ const Navbar = () => {
               </svg>
             </div>
           </div>
-           <h1 className="font-semibold">DigiTools</h1>
+          <h1 className=" font-bold bg-gradient-to-r from-purple-500 to-indigo-500 bg-clip-text text-transparent text-2xl">
+            DigiTools
+          </h1>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -30,24 +34,34 @@ const Navbar = () => {
               <a> Products</a>
             </li>
             <li>
-            <a>Features</a>
+              <a>Features</a>
             </li>
             <li>
               <a>Pricing</a>
             </li>
             <li>
-                <a>Testimonials</a>
+              <a>Testimonials</a>
             </li>
             <li>
-                <a>FAQ</a>
+              <a>FAQ</a>
             </li>
           </ul>
         </div>
+
         <div className="navbar-end">
-          <a className="btn">Get stared</a>
+          <div className="flex items-center gap-4">
+            <AiOutlineShoppingCart />
+            <span className="cursor-pointer text-gray-700 hover:text-purple-500">
+              Login
+            </span>
+
+            <button className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white px-5 py-2 rounded-full font-semibold">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
